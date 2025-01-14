45c3681a86b82b2f7714754e63d3a291de8c0788
Adam Dingman <adam@adamdingman.net>
2025-01-14 15:29:42 -0500

    update sha256

--- s3s.rb
+++ s3s.rb
@@ -2,7 +2,7 @@ class S3s < Formula
   desc "AWS S3 Size Utility"
   homepage "https://github.com/apdingman/homebrew-s3s"
   url "https://github.com/apdingman/homebrew-s3s/releases/download/v0.1.0/s3s-v0.1.0-darwin-amd64.tar.gz"
-  sha256 "f1a2269332eb8c37408bb1f5c8c8ad99cace2c90f919b8fc78c0245886839bc8"
+  sha256 "676c971cce27de53d2d36d9dcc66d9db3e6cea416cc1306b3ee936abdcc7131e"
   version "0.1.0"
 
   def install
