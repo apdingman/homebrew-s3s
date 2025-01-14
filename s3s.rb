9b846ce5cb0633abf3dc15770c09bfaa78e42e55
Adam Dingman <adam@adamdingman.net>
2025-01-14 15:21:34 -0500

    fix typo

--- s3s.rb
+++ s3s.rb
@@ -1,7 +1,7 @@
 class S3s < Formula
   desc "AWS S3 Size Utility"
   homepage "https://github.com/apdingman/homebrew-s3s"
-  url "https://github.com/apdingman/homebrew-s3s/releases/download/v0.1.0/s3s-v0.1.0-darwin-amd64.tar.gz"
+  url "https://github.com/apdingman/homebrew-s3s/releases/download/0.1.0/s3s-v0.1.0-darwin-amd64.tar.gz"
   sha256 "f1a2269332eb8c37408bb1f5c8c8ad99cace2c90f919b8fc78c0245886839bc8"
   version "0.1.0"
 
